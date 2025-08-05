b0847d794cd01df39b52a7b721103db4db0c24a2
HyperWorth <pokemonash33@hotmail.com>
2025-08-05 17:43:44 +0000

    com

--- logic/gemini_api.py
+++ logic/gemini_api.py
@@ -1,13 +1,12 @@
 import os
 from google import genai
 from google.genai import types
-from dotenv import load_dotenv
 from logger import logger  # logger'ı ekledik
-
+import streamlit as st
 # Ortak istemci
-load_dotenv()
+
 client = genai.Client(
-    api_key=os.getenv("GEMINI_API_KEY"),
+    api_key=(st.secrets["GEMINI_API_KEY"]),
 )
 
 
